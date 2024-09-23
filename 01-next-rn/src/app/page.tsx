37be301c5998667128fa9c26a4c99341aba60b14
haiduong-00 <dinhhaiduong203@gmail.com>
2024-09-23 23:01:34 +0700

    init project

---
+++ 01-next-rn/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Hello world nextjs with DuongDinhHai
+      <div>
+        <Button type="primary">Add new</Button>
+      </div>
+    </div>
+  );
+}
